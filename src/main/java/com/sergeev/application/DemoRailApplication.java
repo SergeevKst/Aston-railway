d1389d32fac637e8c553d505bf123a01ff726449
Kst <andrewsergeev98@gmil.com>
2023-04-28 19:05:18 +0300

    Initial commit

---
+++ src/main/java/com/sergeev/application/DemoRailApplication.java
@@ -0,0 +1,13 @@
+package com.sergeev.application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoRailApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoRailApplication.class, args);
+    }
+
+}
